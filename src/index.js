89902c1fe787ea9b49a3eee4bd91b8ded6a71996
Sebastian <cbastian97araque@gmail.com>
2020-12-24 17:19:29 -0500

    ha ocurrido un error al importar la imagen de la nave, así que no sé si la posición esta bien establecida, por ahora buscar el error de importación

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import { nave } from './nave'; // se importa el objeto de la nave
 
+    document.addEventListener('keydown', (event) => {
+        console.log(event);
+    });
+
 new nave(); 
--- src/nave.js
+++ src/nave.js
@@ -1,10 +1,26 @@
-import naveImagen from '../img/nave.png';
+import naveImg from '../img/nave.png'; //ruta de la imagen de la nave
 
 // objeto nave que se importará en el index.js
 export class nave { 
     constructor(){
         this.el = document.createElement('img');
-        this.el.src = naveImagen;
-        document.body.appendChild(this.img);
+        this.el.src = naveImg; // se establece la imagen de la nave
+        document.body.appendChild(this.el);
+
+        this.el.className = 'nave'; //clase que se manipula con CSS
+
+        this.setX(window.innerWidth / 2);  //posicionamiento sobre el eje X
+        this.setY(window.innerHeight - 80); //posicionamiento sobre el eje Y
+    }
+
+    // se ejecuta la configuración de la posición X y Y
+    setX(x) {
+        this.x = x;
+        this.el.style.left = `${this.x}px`; 
+    }
+
+    setY(y) {
+        this.y = y;
+        this.el.style.top = `${this.y}px`;
     }
 }
