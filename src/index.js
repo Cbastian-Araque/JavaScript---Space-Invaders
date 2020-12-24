d1472dfbcf705ff37c65deb62fc41cf7ac15fa22
Sebastian <cbastian97araque@gmail.com>
2020-12-24 16:08:50 -0500

    se exporta la imagen de la nave al DOM

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import { nave } from './nave'; // se importa el objeto de la nave
+
+new nave(); 
--- src/nave.js
+++ src/nave.js
@@ -1,6 +1,10 @@
+import naveImagen from '../img/nave.png';
+
 // objeto nave que se importará en el index.js
 export class nave { 
     constructor(){
-        
+        this.el = document.createElement('img');
+        this.el.src = naveImagen;
+        document.body.appendChild(this.img);
     }
 }
